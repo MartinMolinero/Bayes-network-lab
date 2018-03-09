1d1aea7d781c5d4e3430aabb089b88b6bdc74307
MartinMolinero <a01204818@itesm.mx>
2018-03-09 14:55:42 -0600

    read nodes

--- bayes.py
+++ bayes.py
@@ -1,15 +1,32 @@
+import fileinput
+import sys
 
 class Node:
-    def __init__(self, name):
+    def __init__(self, name, parents):
         self.name = name
+        self.parents = parent
 
 class Network:
+    def __init__(self, nodes):
+        self.nodes = nodes
 
 
 
 
 
 
-import fileinput
-for line in fileinput.input()
-    nodes.push(line.split(','))
+def main():
+    nodes = []
+    inputnodes = sys.stdin.readline().rstrip()
+    inputnodes = inputnodes.replace(' ', '')
+    nodes = inputnodes.split(',')
+    for node in nodes:
+        
+    print(nodes)
+    inputprobs = int(sys.stdin.readline().rstrip())
+    print("probs" + str(inputprobs))
+    for i in range(0,inputprobs):
+
+
+if __name__ == "__main__":
+    main()
