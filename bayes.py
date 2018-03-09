90419c34c1b9c94919290813e2301d3a4b8c4526
MartinMolinero <a01204818@itesm.mx>
2018-03-09 16:41:39 -0600

    find and more methods

--- bayes.py
+++ bayes.py
@@ -2,14 +2,33 @@ import fileinput
 import sys
 
 class Node:
+    parents = []
+    name = ""
+    ptable = {}
     def __init__(self, name, parents):
         self.name = name
         self.parents = parents
 
+    def setParents(self, parents):
+        self.parents = parents
+
+    def setName(self, name):
+        self.name = name
+
+
+
+
+
+
 class Network:
+    nodes = []
     def __init__(self, nodes):
         self.nodes = nodes
 
+    def find(self, name):
+        for node in self.nodes:
+            if node.name == name:
+                return node
 
 
 
@@ -22,14 +41,20 @@ def main():
     inputnodes = inputnodes.replace(' ', '')
     nodes = inputnodes.split(',')
     for i in range(0, len(nodes)):
-        auxnode = Node(nodes[i], None)
-        nodes[i] = auxnode
+        nodes[i] = Node(nodes[i], None)
         print(nodes[i].__dict__)
-    print(nodes)
+    net = Network(nodes)
+    print(net.__dict__)
     inputprobs = int(input())
     probs = []
     for i in range(0,inputprobs):
-        probs[i] = input()
+        probs.append(input())
+
+    inputqueries = int(input())
+    queryarr = []
+    for i in range(inputqueries):
+        queryarr.append(input())
+    #print(queryarr)
 
 
 if __name__ == "__main__":
