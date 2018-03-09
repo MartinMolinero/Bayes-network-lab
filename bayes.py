24c2e1b4c464e7037b6e0308d8b939d4ee64506f
MartinMolinero <a01204818@itesm.mx>
2018-03-09 12:57:13 -0600

    simple class def

---
+++ bayes.py
@@ -0,0 +1,15 @@
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+class Network:
+
+
+
+
+
+
+import fileinput
+for line in fileinput.input()
+    nodes.push(line.split(','))
